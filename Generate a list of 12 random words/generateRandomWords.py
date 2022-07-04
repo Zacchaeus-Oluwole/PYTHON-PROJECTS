9a0b8386e314d89830342e81105426246686c684
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-07-04 11:10:22 +0100

    Generate 12 random words

---
+++ Generate a list of 12 random words/generateRandomWords.py
@@ -0,0 +1,24 @@
+'''
+This program generates twelve random words from wordlist.txt and store it in scanned.txt file.
+It enables you to enter the number of list you want
+'''
+
+import random
+
+word = open("wordlist.txt")
+lineword = word.read()
+listTxt = lineword.splitlines()
+
+txtList = []
+get = int(input("Enter number of list : "))
+# open file in write mode
+with open('scanned.txt', 'w') as fp:
+    while True:
+        randChoices = random.choices(listTxt, k= 12)
+        txtList.append(randChoices)
+        if len(txtList) == get:
+            for list in txtList:
+                print(list)
+                # write each item on a new line
+                fp.write("%s\n" % list)
+            break
