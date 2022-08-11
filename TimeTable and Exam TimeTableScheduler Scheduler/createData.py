8d7757242013c0d9edf7966fc6bf041454dc722f
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-08-11 20:00:14 +0100

    ...

--- TimeTable and Exam TimeTableScheduler Scheduler/createData.py
+++ TimeTable and Exam TimeTableScheduler Scheduler/createData.py
@@ -1,7 +1,8 @@
 # importing pandas as pd
 import pandas as pd
+import random
 
-# list of name, degree, score
+# list of department, level, courses
 dept = ["MTE", "MEE", "CPE", "EEE","CVE"]
 level = [500, 400, 300, 200, 100]
 courses = []
@@ -43,18 +44,31 @@ for i in range(100):
 
 # A dictionary of lists
 
-dict = {'Number': numberList, "Level": levelList, "Dept": deptList, 'Courses':coursesList,}
+dict = {'Number': numberList, "Level": levelList, "Dept": deptList, 'Courses': coursesList,}
 
-# for i in dict["Courses"]:
-#     for v in i:
-#         print(v)
-	
 df = pd.DataFrame(dict)
+df.to_csv('data.csv')
 print(df.to_markdown())
+
+
+# Create courses and Units
+cu = set()
+units = []
+c1 = 0
+c2 = 0
 
 for i in df["Courses"]:
     for v in i:
-        print(v)
+        c1 +=1
+        cu.add(v)
+
+cuList = sorted(list(cu))
+for i in cuList:
+    rand = random.randint(1,3)
+    units.append(rand)
+cuDict = {"Courses":cuList, "Units":units}
 
 # saving the dataframe
-df["Courses"].to_csv('data.csv')
+dt = pd.DataFrame(cuDict)
+dt.to_csv('coursesUnit.csv')
+print(dt.to_markdown())
