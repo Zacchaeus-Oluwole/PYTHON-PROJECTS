5cd1d0b63763bfeca7d3e60deef28ca387df1d72
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-08-14 09:38:29 +0100

    ...

--- TimeTable and Exam TimeTableScheduler Scheduler/test.py
+++ TimeTable and Exam TimeTableScheduler Scheduler/test.py
@@ -1,7 +1,7 @@
-v = "['hello', 'good', 'how', 'what']"
-# v = l.append(b)
-
-
-
-for i in v.strip('"[],'):
-    print(i)
+d =  {"A":[1,2,3,4], "B":['one','two','three','four'], "C":[1,2,'three','four']}
+print(d[0:1])
+# d['A'][2] = 6
+# print(d)
+# d['B'][3] = 8
+# print(d)
+# print(len(d['A'][:2]))
