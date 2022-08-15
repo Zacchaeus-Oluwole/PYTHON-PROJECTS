9a4f4c1119b4c66c90620451735cc0c48681c01a
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-08-15 05:11:24 +0100

    ...

--- TimeTable and Exam TimeTableScheduler Scheduler/test.py
+++ TimeTable and Exam TimeTableScheduler Scheduler/test.py
@@ -1,7 +1,15 @@
-d =  {"A":[1,2,3,4], "B":['one','two','three','four'], "C":[1,2,'three','four']}
-print(d[0:1])
+# d =  {"A":[1,2,3,4], "B":['one','two','three','four'], "C":[1,2,'three','four']}
+# print(d[0:1])
 # d['A'][2] = 6
 # print(d)
 # d['B'][3] = 8
 # print(d)
 # print(len(d['A'][:2]))
+
+import random
+
+a = random.randint(2,12)
+
+print(a)
+a -= 2
+print(a)
