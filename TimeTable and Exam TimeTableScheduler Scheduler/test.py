f89192103a9b89d0de23a70e6ff82c9033367da7
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-08-24 11:42:49 +0100

    ...

--- TimeTable and Exam TimeTableScheduler Scheduler/test.py
+++ TimeTable and Exam TimeTableScheduler Scheduler/test.py
@@ -1,24 +1,27 @@
 import pandas as pd
+import ast
 d =  {
-        "A":[
-                [1,2,3,4],
-                ['one','two','three','four'],
-                ['one','two','three','four'],
-                [1,2,3,4]
+        "Morning\n\n08:00am 09:00am 10:00am 11:00am":[
+                [4,2,3,4],
+                ['three','two','three','four'],
+                ['three','two','three','three'],
+                [8,2,0,4]
             ], 
-        "B":[
+        "Afternoon\n\n11:30am 12:30pm 01:30pm 02:30pm":[
+                ['one','three','three','four'],
                 ['one','two','three','four'],
-                ['one','two','three','four'],
-                [1,2,3,4],
-                [1,2,3,4]
+                [1,9,3,4],
+                [1,9,3,4]
             ], 
-        "C":[
-                [1,2,'three','four'],
-                [1,2,3,4],
-                [1,2,'three','four'],
-                ['one','two','three','four']
+        "Evening\n\n03:00pm 04:00pm 05:00pm 06:00pm":[
+                [1,9,'three','four'],
+                [7,2,3,6],
+                [1,8,'three','four'],
+                ['three','two','three','four']
             ]
         }
+
+
 df = pd.DataFrame(d)
 # print(d[0:1])
 print(df.to_markdown())
