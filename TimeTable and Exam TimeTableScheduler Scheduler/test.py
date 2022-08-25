282f8f8e58c3400030d05e420e45c16591f9c6dd
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-08-25 18:25:34 +0100

    ...

--- TimeTable and Exam TimeTableScheduler Scheduler/test.py
+++ TimeTable and Exam TimeTableScheduler Scheduler/test.py
@@ -1,26 +1,46 @@
 import pandas as pd
+# d =  {
+#         "Morning\n\n08:00am 09:00am 10:00am 11:00am":[
+#                 [4,2,3,4],
+#                 ['three','two','three','four'],
+#                 ['three','two','three','three'],
+#                 [8,2,0,4]
+#             ], 
+#         "Afternoon\n\n11:30am 12:30pm 01:30pm 02:30pm":[
+#                 ['one','three','three','four'],
+#                 ['one','two','three','four'],
+#                 [1,9,3,4],
+#                 [1,9,3,4]
+#             ], 
+#         "Evening\n\n03:00pm 04:00pm 05:00pm 06:00pm":[
+#                 [1,9,'three','four'],
+#                 [7,2,3,6],
+#                 [1,8,'three','four'],
+#                 ['three','two','three','four']
+#             ]
+#         }
+
 d =  {
-        "Morning\n\n08:00am 09:00am 10:00am 11:00am":[
-                [4,2,3,4],
-                ['three','two','three','four'],
-                ['three','two','three','three'],
-                [8,2,0,4]
+        "A":[
+               [' '],
+               [' ']
             ], 
-        "Afternoon\n\n11:30am 12:30pm 01:30pm 02:30pm":[
-                ['one','three','three','four'],
-                ['one','two','three','four'],
-                [1,9,3,4],
-                [1,9,3,4]
+        "B":[
+                [' '],
+                [' ']
             ], 
-        "Evening\n\n03:00pm 04:00pm 05:00pm 06:00pm":[
-                [1,9,'three','four'],
-                [7,2,3,6],
-                [1,8,'three','four'],
-                ['three','two','three','four']
+        "C":[
+                [' '],
+                [' ']
             ]
-        }
-m = d["Morning\n\n08:00am 09:00am 10:00am 11:00am"]
-print(m[1][2])
+    }
+for i in range(0,10):
+    d['A'].append([' '])
+    print(type(d['A']))
+    d['B'].append("H")
+    d['C'].append("I")
+# m = d["Morning\n\n08:00am 09:00am 10:00am 11:00am"]
+# print(m[1][2])
 
 df = pd.DataFrame(d)
 # print(d[0:1])
