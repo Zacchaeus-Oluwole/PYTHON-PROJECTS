d45f2bec8e1d77bc1c142111424c73274cddb9fe
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-09-03 18:58:54 +0100

    ...

--- TimeTable and Exam TimeTableScheduler Scheduler/test.py
+++ TimeTable and Exam TimeTableScheduler Scheduler/test.py
@@ -1,4 +1,4 @@
-# PRINT THE PREVIOUS VALUE
+# PRINT THE PREVIOUS VALUE --- LAST EVENT REMINDER
 # valueX = 0
 # while True:
 #     value = int(input('Enter'))
@@ -9,21 +9,24 @@
 #     valueX = value
 
 
+# RE-ARRANGE BY SELECTIVE GROUPING OF DIFFERENT VALUES
 
 x = ['B','B','B','B','D','D','D','D','A','A','A','A','C','C','C','C','E','E','E','E']
 l = []
 i = 0
 v = 1
 
+# RE-ARRANGE BY SELECTIVE GROUPING OF DIFFERENT VALUES---BY MULTIPLICATION
 # for a in x:
 #     for b in x:
 #         i+=1
-#         if i == v*3:
+#         if i == v*4:
 #             v+=1
 #             l.append(b)
 #     if len(l) == len(x):
 #         break
 
+# RE-ARRANGE BY SELECTIVE GROUPING OF DIFFERENT VALUES---CHECKING THE PAST VALUE
 for a in x:
     for b in x:
         if l == []:
@@ -32,5 +35,6 @@ for a in x:
             l.append(b)
     if len(l) == len(x):
         break
+
 print(x)
 print(l)
