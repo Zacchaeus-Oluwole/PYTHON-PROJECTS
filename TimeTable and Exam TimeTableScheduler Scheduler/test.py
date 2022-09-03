1ea82f9f5722528681f77739d3ff7e5e1080373d
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-09-03 14:00:12 +0100

    cd /home/zacchaeus/Desktop/WORKSPACE/programming/PYTHON/TimeTable\ and\ Exam\ TimeTableScheduler\ Scheduler ; /usr/bin/env /usr/bin/python3 /home/zacchaeus/.vscode/extensions/ms-python.python-2022.12.1/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 43855 -- /home/zacchaeus/Desktop/WORKSPACE/programming/PYTHON/TimeTable\ and\ Exam\ TimeTableScheduler\ Scheduler/test.py

--- TimeTable and Exam TimeTableScheduler Scheduler/test.py
+++ TimeTable and Exam TimeTableScheduler Scheduler/test.py
@@ -1,9 +1,25 @@
-valueX = 0
-while True:
-    value = int(input('Enter'))
-    if value == valueX:
-        print("NaN")
-    else:
-        print(valueX)
-    valueX = value
+# valueX = 0
+# while True:
+#     value = int(input('Enter'))
+#     if value == valueX:
+#         print("NaN")
+#     else:
+#         print(valueX)
+#     valueX = value
+
+x = ['B','B','B','D','D','D','A','A','A','C','C','C']
+l = list()
+i = 0
+v = 1
+for a in x:
+    for b in x:
+        i+=1
+        if i == v*3:
+            v+=1
+            l.append(b)
+    if len(l) == len(x):
+        break
+
+print(x)
+print(l)
 
